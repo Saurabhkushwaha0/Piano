ba6ecb9bb3fe68224dbcf1bf94f28eb5121ccad0
Saurabh kushwaha <94947417+Saurabhkushwaha0@users.noreply.github.com>
2022-11-09 09:49:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+function A0()
+{
+     
+     let A0 = new Audio('piano/A0.mp3');
+	A0.play();
+}
+function A1()
+{
+     let A1 = new Audio('piano/A1.mp3');
+	A1.play();
+}
+function A2()
+{
+     let A2 = new Audio('piano/A2.mp3');
+	A2.play();
+}
+function A3()
+{
+     let A3 = new Audio('piano/A3.mp3');
+	A3.play();
+}
+function A4()
+{
+     let A4 = new Audio('piano/A4.mp3');
+	A4.play();
+}
+function A5()
+{
+     let A5 = new Audio('piano/A5.mp3');
+	A5.play();
+}
+function A6()
+{
+     let A6 = new Audio('piano/A6.mp3');
+	A6.play();
+}
+function A7()
+{
+     let A7 = new Audio('piano/A7.mp3');
+	A7.play();
+}
+function Ab1()
+{
+     let Ab1 = new Audio('piano/Ab1.mp3');
+	Ab1.play();
+}
+function Ab2()
+{
+     let Ab2 = new Audio('piano/Ab2.mp3');
+	Ab2.play();
+}
+function Ab3()
+{
+     let Ab3 = new Audio('piano/Ab3.mp3');
+	Ab3.play();
+}
+function Ab4()
+{
+     let Ab4 = new Audio('piano/Ab4.mp3');
+	Ab4.play();
+}
